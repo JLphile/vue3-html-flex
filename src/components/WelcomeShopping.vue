1f111984846e7812ba9ada8207c49dbc2478c730
JLphile <lkyz001@outlook.com>
2022-10-30 23:31:45 +0800

    flash sale

--- src/components/WelcomeShopping.vue
+++ src/components/WelcomeShopping.vue
@@ -24,9 +24,99 @@
       </ul>
     </div>
     <!-- 导航栏 -->
-    <div class="index-nav"></div>
+    <div class="index-nav">
+      <ul>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+        <li>
+          <a href="">
+            <div>新品</div>
+            <p>新品专区</p>
+          </a>
+        </li>
+      </ul>
+    </div>
     <!-- 秒杀 -->
-    <div class="index-seckill"></div>
+    <div class="index-seckill">
+      <div class="seckill-count">
+        <i class="icon-shouye"></i>
+        <span>限时秒杀</span>
+      </div>
+      <div class="seckill-time">
+        <ul>
+          <li>
+            <p>0:00</p>
+            <b>抢购中</b>
+          </li>
+          <li>
+            <p>10:00</p>
+            <b>抢购中</b>
+          </li>
+          <li>
+            <p>14:00</p>
+            <b>抢购中</b>
+          </li>
+        </ul>
+      </div>
+      <div class="seckill-column">
+        <div class="column-left">
+          <a href="#">
+            <img src="@/assets/images/seckill.png" alt="" />
+            <span>秒杀</span>
+          </a>
+        </div>
+        <div class="column-right">
+          <h5><a href="#">农家养土鸡蛋现捡新鲜谷物虫草柴鸡蛋盒装 30枚装</a></h5>
+          <p>秒杀价</p>
+          <div class="column-list">
+            <i>￥</i>
+            <b>23.9</b>
+            <s>￥44.9</s>
+            <a href="#">立即抢购</a>
+          </div>
+        </div>
+      </div>
+    </div>
     <!-- 商品展示区 -->
     <div class="index-main"></div>
     <!-- 页面底部 -->
@@ -84,5 +174,133 @@
 .index-banner ul li {
   color: #fff;
   margin: 0 1px;
+}
+/* 导航栏 */
+.index-nav {
+  width: 100%;
+}
+.index-nav a div {
+  background: url(@/assets/images/bgc.png) no-repeat center top;
+  width: 100%;
+  height: 66px;
+  color: #fff;
+  line-height: 50px;
+}
+.index-nav ul {
+  display: flex;
+  flex-wrap: wrap;
+  text-align: center;
+}
+.index-nav ul li {
+  flex: 0 1 25%;
+  margin-bottom: 10px;
+}
+/* 秒杀 */
+.index-seckill {
+  background-color: #fe6b38;
+  border-radius: 10px;
+  margin: 10px 0;
+  padding-bottom: 10px;
+}
+.seckill-count {
+  background-color: #fce5c6;
+  width: 25%;
+  border-top-left-radius: 10px;
+  padding: 5px;
+  color: #fe6b38;
+  font-size: 14px;
+}
+.seckill-time ul {
+  display: flex;
+  text-align: center;
+  margin: 10px 10px 0 10px;
+}
+.seckill-time ul li {
+  flex: 0 1 23%;
+  padding: 10px 0;
+}
+.seckill-time ul li p {
+  color: #fff;
+  font-size: 18px;
+}
+.seckill-time ul li b {
+  color: #fff;
+  font-weight: 400;
+}
+.seckill-time ul li:hover {
+  background-color: #faa73f;
+  /* border-top-left-radius: 8px; */
+  /* border-top-right-radius: 8px; */
+  border-radius: 8px 8px 0 0;
+}
+.seckill-time ul li:hover b {
+  background-color: #fff;
+  color: red;
+  border-radius: 7px;
+  padding: 0 5px;
+}
+.seckill-column {
+  display: flex;
+  background-color: #fff;
+  border: 5px solid #fef9ec;
+  border-radius: 8px;
+  margin: 0 10px;
+}
+.column-left img {
+  max-height: 100px;
+}
+.column-left {
+  position: relative;
+  flex: 1;
+}
+.column-left span {
+  position: absolute;
+  top: 2px;
+  left: 1px;
+  background-color: #fd9639;
+  color: #fff;
+  padding: 0 5px;
+}
+.column-right {
+  flex: 2;
+}
+.column-right h5 {
+  font-size: 16px;
+}
+.column-right h5 a {
+  color: black;
+}
+.column-right p {
+  background-color: #fd9639;
+  color: #fff;
+  width: 42px;
+  text-align: center;
+  margin-top: 15px;
+}
+.column-list {
+  position: relative;
+}
+.column-list i {
+  font-style: normal;
+  color: #fe4557;
+}
+.column-list b {
+  color: #fe4557;
+  font-size: 18px;
+  margin-left: -3px;
+}
+.column-list s {
+  color: #999;
+}
+.column-list a {
+  background-color: #fe4557;
+  color: #fff;
+  border-radius: 15px;
+  padding: 4px 15px;
+  font-size: 16px;
+  font-weight: 700px;
+  position: absolute;
+  right: 0;
+  bottom: 4px;
 }
 </style>

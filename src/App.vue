291fd39da14190687048762d49823149d55825e9
JLphile <lkyz001@outlook.com>
2022-11-01 10:23:38 +0800

    home-circle-loading

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div>
     <!-- <HelloWorld /> -->
     <!-- <ShoppingMall /> -->
-    <WelcomeShopping />
+    <!-- <WelcomeShopping /> -->
+    <HomeView />
   </div>
 </template>
 
 <script setup lang="ts">
 // import HelloWorld from "./components/HelloWorld.vue";
 // import ShoppingMall from "./components/ShoppingMall.vue";
-import WelcomeShopping from "./components/WelcomeShopping.vue";
+// import WelcomeShopping from "./components/WelcomeShopping.vue";
+import HomeView from "./views/HomeView.vue";
 </script>
 
 <style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home-view">
+    <div>
+      <CircleLoading></CircleLoading>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import CircleLoading from "@/components/CircleLoading.vue";
+</script>
+
+<style scoped>
+.home-view {
+  box-sizing: content-box;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(3, 1fr);
+  height: 100vh;
+  justify-items: center;
+  align-items: center;
+  max-width: 800px;
+  margin: 0 auto;
+}
+.home-view > div {
+  transform: scale(1.5);
+}
+</style>
